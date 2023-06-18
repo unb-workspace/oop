62a6f48427c235bcde90bb0b07ed66a3f65a4e02
Ana Clara <abarbosaborges17@gmail.com>
2023-06-18 19:04:50 -0300

    create the component TextField

---
+++ src/views/components/TextField.java
@@ -0,0 +1,18 @@
+package components;
+
+import java.awt.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+
+public class TextField extends JTextField{
+	
+	public TextField() {
+		super();
+		this.setPreferredSize(new Dimension(200, 30));
+		this.setBackground(new Color(199, 194, 194));
+		this.setFont(new Font("Arial", Font.BOLD, 15));
+		this.setBorder(BorderFactory.createEmptyBorder());
+	}
+	
+}
