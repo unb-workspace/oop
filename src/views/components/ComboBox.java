54a3284715bd750313a7fb2341ac994817336b24
Caio <caiofeliperro@gmail.com>
2023-06-21 19:46:09 -0300

    create comboBox and textLabel classes

---
+++ src/views/components/ComboBox.java
@@ -0,0 +1,12 @@
+package components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ComboBox extends JComboBox<Object> {
+    public ComboBox(Object[] values) {
+        super(values);
+        this.setBackground(new Color(199, 194, 194));
+        this.setBorder(BorderFactory.createEmptyBorder());
+    }
+}
---
+++ src/views/components/TextLabel.java
@@ -0,0 +1,11 @@
+package components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextLabel extends JLabel {    
+    public TextLabel(String text) {
+        super(text);
+        this.setFont(new Font("Arial", Font.BOLD, 15));
+    }
+}
