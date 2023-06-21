4921d34e0558ab56c11b64dd21268338904d1bfd
Caio <caiofeliperro@gmail.com>
2023-06-21 08:20:37 -0300

    add new job list methods

--- src/controller/JobController.java
+++ src/controller/JobController.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class JobController {
     private final static String[] modalities = {"Presencial", "Hibrido", "Remoto"};
     
@@ -16,8 +18,79 @@ public class JobController {
     }
 
     public Job createJob(String name, String occupationArea, int salary, String modality, int workload) {
-        Job job = new Job(name, salary, workload, modality, occupationArea);
+        return new Job(name, salary, workload, modality, occupationArea);
+    }
 
-        return job;
+    public ArrayList<String> getJobsList(ArrayList<Company> companies) {
+        ArrayList<String> jobList = new ArrayList<>();
+
+        for(Company company : companies) {
+            for(Job job : company.getJobs()) {
+                jobList.add("Nome da vaga: " + job.getName()
+                + "\nNome da empresa: " + company.getName()
+                + "\nRepresentante: " + company.getRepresentant()
+                + "\nModalidade: " + job.getModality()
+                + "\nÁrea de Ocupação: " + job.getOccupationArea());
+            }
+        }
+
+        return jobList;
+    }
+
+    public ArrayList<String> filterJobsByCompany(String companyName, ArrayList<Company> companies) {
+        ArrayList<String> jobList = new ArrayList<>();
+
+        for(Company company : companies) {
+            if(!company.getName().toLowerCase().contains(companyName.toLowerCase()))
+                continue;
+
+            for(Job job : company.getJobs()) {
+                jobList.add("Nome da vaga: " + job.getName()
+                        + "\nNome da empresa: " + company.getName()
+                        + "\nRepresentante: " + company.getRepresentant()
+                        + "\nModalidade: " + job.getModality()
+                        + "\nÁrea de Ocupação: " + job.getOccupationArea());
+            }
+        }
+
+        return jobList;
+    }
+
+    public ArrayList<String> filterJobsByName(String jobName, ArrayList<Company> companies) {
+        ArrayList<String> jobList = new ArrayList<>();
+
+        for(Company company : companies) {
+            for(Job job : company.getJobs()) {
+                if(!job.getName().toLowerCase().contains(jobName.toLowerCase()))
+                    continue;
+
+                jobList.add("Nome da vaga: " + job.getName()
+                        + "\nNome da empresa: " + company.getName()
+                        + "\nRepresentante: " + company.getRepresentant()
+                        + "\nModalidade: " + job.getModality()
+                        + "\nÁrea de Ocupação: " + job.getOccupationArea());
+            }
+        }
+
+        return jobList;
+    }
+
+    public ArrayList<String> filterJobsByRegion(String region, ArrayList<Company> companies) {
+        ArrayList<String> jobList = new ArrayList<>();
+
+        for(Company company : companies) {
+            if(company.getAddress().getState() != region)
+                continue;
+
+            for(Job job : company.getJobs()) {
+                jobList.add("Nome da vaga: " + job.getName()
+                        + "\nNome da empresa: " + company.getName()
+                        + "\nRepresentante: " + company.getRepresentant()
+                        + "\nModalidade: " + job.getModality()
+                        + "\nÁrea de Ocupação: " + job.getOccupationArea());
+            }
+        }
+
+        return jobList;
     }
 }
