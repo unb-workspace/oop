da596cbf26c6132fdeb248b23e74b64331db930d
Caio <caiofeliperro@gmail.com>
2023-06-23 18:39:17 -0300

    add some examples

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,30 @@ public class Main {
         JobController jobController = new JobController();
         EmployerController employerController = new EmployerController();
         
-        Employer employer = employerController.createEmployer("João", "joao", "123", "Gerente");
-        Company company = companyController.createCompany("Empresa 1", "joao@gmail.com", "SP", "São Paulo", "Rua 1", "TI", "joao");
-        Job job = jobController.createJob("Vaga 1", "1", 4000, "Presencial", 40);
+        Company company = companyController.createCompany("Apple", "contato@apple.com", "Amapá", "Macapá", "Rua das Araucárias", "Tecnologia", "stevetrabalhos");
+        Job job = jobController.createJob("Designer", "Imagem e Vídeo", 17534, "Híbrido", 6);
+        Employer employer = employerController.createEmployer("Steve Trabalhos", "stevetrabalhos", "dinheiro123", "Fundador");
         company.addJob(job);
         employer.setCompany(company);
-        
+
+        Company company2 = companyController.createCompany("Google", "contato@google.com", "Minas Gerais", "Belo Horizonte", "Rua das Américas", "Tecnologia", "larrypaginas");
+        Job job2 = jobController.createJob("Engenheiro de Software A3", "TI", 18785, "Híbrido", 8);
+        Employer employer2 = employerController.createEmployer("Larry Páginas", "larrypaginas","segredo" , "Fundador");
+        company2.addJob(job2);
+        employer2.setCompany(company2);
+
+        Company company3 = companyController.createCompany("SpaceX", "contato@tesla.com", "São Paulo", "São Paulo", "Avenida Paulista", "Tecnologia", "elonmosquito");
+        Job job3 = jobController.createJob("Fogueteiro", "Projetista", 26988, "Presencial", 10);
+        Employer employer3 = employerController.createEmployer("Elon Mosquito", "elonmosquito", "marte123", "Fundador");
+        company3.addJob(job3);
+        employer3.setCompany(company3);
+
+        Company company4 = companyController.createCompany("Magalu", "contato@magalu.com.br", "São Paulo", "Franca", "Rua Francisco Marques", "Lojista", "luizavestino");
+        Job job4 = jobController.createJob("Vendedor","Comécio", 2466, "Presencial", 12);
+        Employer employer4 = employerController.createEmployer("Luiza Vestino", "luizavestino", "empoderada123", "Fundadora");
+        company4.addJob(job4);
+        employer4.setCompany(company4);
+
         new Home();
     }
 }
